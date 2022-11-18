28213d644fe18ab705c209c6c4b4a8dec5bf0a83
shunsuke6 <sawabes@ns.aucnet.co.jp>
2022-11-18 22:37:46 +0900

    add: add plugin

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nvim-yoshi
+
+![nvim-yoshi](https://user-images.githubusercontent.com/84017923/202717273-db4d449c-be07-4e9d-ba90-dde0f77687dd.gif)
+
+## Depend on
+
+[nui.nvim](https://github.com/MunifTanjim/nui.nvim)
+
+## Inspire
+
+[vim-yoshi](https://github.com/mattn/vim-yoshi/)
---
+++ lua/yoshi.lua
@@ -0,0 +1,47 @@
+local Popup = require("nui.popup")
+local event = require("nui.utils.autocmd").event
+
+local M = {}
+local yoshi = {
+    "             /＼  ／ヽ",
+    "            {／￣￣￣ヽ!",
+    "            ∠＿＿╋＿＿ｊ",
+    " :w ヨシ!   / (.)八(.)  ヽ",
+    "           ｛=/(人_)=|´￣)｀ヽ",
+    "            ＼ { {_,ﾉ ﾉ   //~ `",
+    "        ⊂￣ヽ_＞―――‐''’,〈   (＿)",
+    "         `ヘ(＿ ィ r―‐γ   !",
+    "              _,ﾉ j   |   |",
+    "            ｛   ｛    ﾉ  /＼",
+    "             ＼ス￣￣￣lしｲ＼ ＼",
+    "            (￣ ）     j /   ＼_j＼",
+    "             ￣￣     ( ｀ヽ   ＼__)",
+    "                       ＼__ﾉ",
+}
+
+M.popup = Popup({
+    focusable = false,
+    border = {
+        style = "rounded",
+    },
+    relative = "cursor",
+    position = {
+        row = -10,
+        col = 10,
+    },
+    size = {
+        width = 49,
+        height = 14,
+    },
+})
+function M.show()
+    M.popup:mount()
+    vim.fn.timer_start(2000, function()
+        M.popup:unmount()
+    end)
+
+    vim.api.nvim_buf_set_lines(M.popup.bufnr, 0, 1, false, yoshi)
+end
+
+return M
+-- vim.cmd([[autocmd BufWritePost * :luafile %]])
